4b7c3a434dfb05ce1f20a28268d96aaabecf7efa
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-02-03 11:12:17 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+const country = 'bangladesh';
+const age = 52;
+const inIndependent = true;
+const student = { id : 121, age: 11, name : 'agun'};
+const friends = [13, 14, 11, 17, 21, 16, 15,20];
+
+console.log(typeof country);
+console.log(typeof age);
+
