8ace73adbb7413de48e9e791ca0547e9af8a4ef0
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-02-03 11:30:16 +0600

    first commit

--- array.js
+++ array.js
@@ -3,7 +3,14 @@ const age = 52;
 const inIndependent = true;
 const student = { id : 121, age: 11, name : 'agun'};
 const friends = [13, 14, 11, 17, 21, 16, 15,20];
+function add(num1, num2){
+    return num1 + num2;
+}
 
 console.log(typeof country);
 console.log(typeof age);
-
+console.log(typeof inIndependent);
+console.log(typeof student);
+//check array using Array.isArray
+console.log(Array.isArray(friends));
+console.log(typeof add);
